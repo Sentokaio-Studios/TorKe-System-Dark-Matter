8baf05270addef5604fd04cc7ea2619a055b74bc
Spray <71204668+sprayx@users.noreply.github.com>
2021-03-20 12:53:52 -0300

    Update BulletControl.cs

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BulletControl : MonoBehaviour
 {
     // Start is called before the first frame update
-    // Start is called before the first frame update
     void Start()
     {
 
