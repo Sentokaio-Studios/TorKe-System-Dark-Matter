e0fc754ecaf7070625ddbac9a3e42ecb70d756b4
Sprayapenas <sprayhasswag@gmail.com>
2021-03-19 22:57:53 -0300

    TorKe System | Arquivos iniciais

---
+++ Assets/Scripts/Enemy/Kotoro Abusame/ClackerScripts/ClackerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClackerMovement : MonoBehaviour
+{
+    public GameObject object1, object2;
+
+    public float speed;
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        // Calculate direction vector.
+        Vector3 dirction = object1.transform.position - object2.transform.position;
+
+        // Normalize resultant vector to unit Vector.
+        dirction = -dirction.normalized;
+
+        // Move in the direction of the direction vector every frame.
+        object1.transform.position += dirction * Time.deltaTime * speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Library/Collab/Download/Assets/triggerLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class triggerLevel : MonoBehaviour
+{
+    public int sceneValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                SceneManager.LoadScene(sceneValue);
+            }
+        }
+    }
+}
